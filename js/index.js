b5a2fb4b0c02da2e7dd10db3c69eee2d06bc2685
MinhTriHa <minh.tri.ha@student.howest.be>
2022-10-25 13:36:03 +0200

    Add vite.js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,16 @@
-import JSConfetti from "../node_modules/js-confetti/dist/es/index.js";
+import "../css/style.css";
+
+import JSConfetti from "js-confetti";
 
 const init = () => {
   const $form = document.querySelector("form");
   const jsConfetti = new JSConfetti();
-  
+
   $form.addEventListener("submit", (event) => {
     event.preventDefault();
 
     jsConfetti.addConfetti();
   });
-}
-
+};
 
 init();
